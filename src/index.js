85acbf67bbd550f10f72af591a201bc6d3c69d6c
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-07-30 21:45:30 +0200

    Implement tab switching

--- src/index.js
+++ src/index.js
@@ -9,7 +9,36 @@ const homeTab = buildHomeTab();
 contentDiv.appendChild(homeTab);
 
 const menuTab = buildMenuTab();
+menuTab.classList.add("hidden");
 contentDiv.appendChild(menuTab);
 
 const aboutTab = buildAboutTab();
+aboutTab.classList.add("hidden");
 contentDiv.appendChild(aboutTab);
+
+const homeButton = document.querySelector("#home-button");
+homeButton.addEventListener("click", handleHomeClicked);
+
+const menuButton = document.querySelector("#menu-button");
+menuButton.addEventListener("click", handleMenuClicked);
+
+const aboutButton = document.querySelector("#about-button");
+aboutButton.addEventListener("click", handleAboutClicked);
+
+function handleHomeClicked() {
+    homeTab.classList.remove("hidden");
+    menuTab.classList.add("hidden");
+    aboutTab.classList.add("hidden");
+}
+
+function handleMenuClicked() {
+    homeTab.classList.add("hidden");
+    menuTab.classList.remove("hidden");
+    aboutTab.classList.add("hidden");
+}
+
+function handleAboutClicked() {
+    homeTab.classList.add("hidden");
+    menuTab.classList.add("hidden");
+    aboutTab.classList.remove("hidden");
+}
